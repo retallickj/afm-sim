e13ba0345d0dc6a3891f4a7902dbc3cbaeaea193
Jake Retallick <retallick.jake@gmail.com>
2018-02-15 14:30:55 -0800

    Started Hopping Viewer

---
+++ src/python/viewer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+'''
+Real-time animation of DB arrangements
+'''
+
+__author__      = 'Jake Retallick'
+__copyright__   = 'MIT License'
+__version__     = '1.2'
+__date__        = '2018-02-15'  # last update
+
+
+
+class HoppingAnimation:
+    ''' '''
+
+    def __init__(self, X, Y):
+        '''Initialise the HoppingAnimation instance'''
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    pass
