8cfaa2b4cfadd7b78cbce4e5bb716ad3292390b2
Jake <retallick.jake@gmail.com>
2018-02-22 21:58:50 -0800

    increased bulk hopping threshold a bit

--- src/python/channel.py
+++ src/python/channel.py
@@ -73,8 +73,8 @@ class Bulk(Channel):
     nu_off  = 1.    # maximum rate of hops from the bulk
 
     # energy offsets
-    mu_on   = .15    # local energy at which electrons start hopping onto Bulk
-    mu_off  = .15    # local energy at which electrons start hopping from Bulk
+    mu_on   = .17    # local energy at which electrons start hopping onto Bulk
+    mu_off  = mu_on  # local energy at which electrons start hopping from Bulk
 
     alpha   = 1.e3  # damping factor for kt, higher means sharper transition
 
